1c0a8d5f47689a9ff2c2f12fe768e210c6713cba
NozoDev <kevinroj326@gmail.com>
2024-11-04 16:51:55 -0500

    interfaz

--- main.js
+++ main.js
@@ -8,3 +8,32 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
   showNav.classList.remove("visible");
 });
+
+// formulario
+const form = document.querySelector(".main-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value; // obtengo los usuarios
+  const password = document.getElementById("password").value; // obtengo los password
+
+  const validacionForm = (username, password) => {
+    if (username === "" || password === "") {
+      alert("Por favor, ingrese un nombre de usuario y contraseña");
+      return; // salgo de la función si los campos están vacíos
+    }
+
+    if (password.length < 7) {
+      alert("La contraseña debe tener al menos 7 caracteres");
+      return false; // salgo de la función si la contraseña no tiene la longitud adecuada
+    }
+    return true;
+  };
+  const isValid = validacionForm(password, username);
+  if (isValid) {
+    alert("Usuario registrado");
+
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+  }
+});
